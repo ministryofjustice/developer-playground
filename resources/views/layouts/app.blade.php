176471a1eb1bd1e7d0876d6bad0008aea59e6305
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2023-10-06 17:45:27 +0100

    Upgrade software

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="{{ asset('assets/judiciary-icon.png') }}" type="image/x-icon">
+    <link rel="icon" href="{{ mix('assets/judiciary-icon.png') }}" type="image/x-icon">
 
     <title>{{ config('app.name', 'Tool Procurement Centre | MoJ D&T') }}</title>
 
@@ -12,12 +12,12 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('assets/css/app.css') }}">
 
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     @stack('head')
-    <script src="{{ asset('assets/js/app.js') }}" defer></script>
+    <script src="{{ mix('assets/js/app.js') }}" defer></script>
 </head>
 <body>
 <script>document.body.className = ((document.body.className) ? document.body.className + ' js-enabled' : 'js-enabled');</script>
@@ -34,7 +34,7 @@
 </div>
 <x-footer/>
 
-<script src="{{ asset('assets/js/govuk.js') }}"></script>
+<script src="{{ mix('assets/js/govuk.js') }}"></script>
 <script>
     window.GOVUKFrontend.initAll();
 </script>
